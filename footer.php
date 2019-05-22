b8e6e690cb186a52df11957dc1d1bd74a044b6b6
EgzonaVllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2019-05-22 10:02:03 +0200

    include

---
+++ footer.php
@@ -0,0 +1,92 @@
+<?php 
+
+
+	echo '<footer>
+		
+		<div class="foot">
+
+
+			<div class="fund">
+
+				<a href="#">Buy</a><br>
+				<a href="#">Buy Phones</a><br>
+				<a href="#">Buy Tablets</a><br>
+				<a href="#">Buy MacBooks</a><br>
+				<a href="#">Buy Laptops</a><br>
+				<a href="#">Buy Cameras</a>
+				
+			</div>
+
+			<div class="fund">
+
+				<a href="#">Buy iPhones</a><br>
+				<a href="#">Buy Video Games</a><br>
+				<a href="#">Buy Watches & Wearables</a><br>
+				<a href="#">Buy Home Tech</a><br>
+				<a href="#">Cheap Phones</a>
+				
+			</div>
+			<div class="fund">
+
+				<a href="#">Sell</a><br>
+				<a href="#">How to sell</a><br>
+				<a href="#">Sell iPhones</a><br>
+				<a href="#">Sell Phone</a><br>
+				<a href="#">Sell Tablet</a><br>
+				<a href="#">Sell Laptop</a>
+
+			</div>
+			<div class="fund">
+
+				<a href="#">Phone Finder</a><br>
+				<a href="#">IMEI/ ESN Check</a><br>
+				<a href="#">Swappa vs eBay</a><br>
+				<a href="#">Swappa vs Craigslist</a><br>
+				<a href="#">Affiliate Program</a>
+
+			</div>
+
+			<div class="fund">
+
+				<a href="#">Helo</a><br>
+				<a href="#">FAQs</a><br>
+				<a href="#">Data Services</a><br>
+				<a href="#">Price Lists</a><br>
+				<a href="#">Broken, Repair</a><br>
+				<a href="#">Protection Plans</a>
+
+			</div>
+
+			<div class="fund">
+
+				<a href="#">About Exoy</a><br>
+				<a href="#">Blog</a><br>
+				<a href="#">Contact us </a><br>
+				<a href="#">Press, Reviews</a><br>
+				<a href="#">Polices, Fees</a><br>
+				<a href="#">Terms of Use</a>
+
+			</div>
+			
+			
+		</div>
+		<br>
+
+		<div class="rights">
+			<h2 class="title" style=" margin:0px 0px 0px 25px;">Exoy</h2>
+			
+			<div class="social_media">
+				<a href="#" class="linkedIn" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+				<a href="#" class="twitter" target="_blank"><i class="fab fa-twitter"></i></a>
+				<a href="#" class="pinterest" target="_blank"><i class="fab fa-pinterest-p"></i></a>
+				<a href="#" class="googleplus" target="_blank"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="rss" target="_blank"><i class="fas fa-rss"></i></a>
+			</div>
+
+			<p style="color: white">&copy;Copyright Exoy&#39;s world - All rights reserved </p>
+		</div>
+
+	</footer>';
+
+
+?>
---
+++ header.php
@@ -0,0 +1,114 @@
+<?php 
+
+
+	echo '<header>
+		<div class="header container">
+			
+			<h2 class="title">Exoy</h2>
+			<input type="text" name="search" placeholder="Search for phones, tablets, laptops and more..."><button class="buto"><img src="img/search.png" width="15px" height="15px"></button>
+			<button class="sell">Sell on Exoy</button>
+			<a href="login.php"><button>Login</button></a>
+			<a href="signup.php"><button>Register</button></a>
+			<button><img src="img/addtocart.png" width="15px" height="15px"></button>
+
+		</div>
+
+		<hr>
+
+		<section class="container">
+			
+			<div class="dropdown">
+
+				<a href="iphones.php"><img src="img/apple.png" width="13px" height="13px"> &nbsp;iPhone</a>
+				<div class="dropdown-content">
+				<a href="iphoneX.php">iPhone X</a>
+				<a href="iPhone8.php">iPhone 8</a>
+				<a href="iPhone8Plus.php">iPhone 8 Plus</a>
+				<a href="iPhone7.php">iPhone 7</a>
+				<a href="iPhone7Plus.php">iPhone 7 Plus</a>
+				<a href="iPhone6s.php">iPhone 6S</a>
+				<a href="iPhone6sPlus.php">iPhone 6S Plus</a>
+				</div>
+
+			</div>	
+
+			<div class="dropdown">
+
+				<a href="#"><img src="img/phone.png" width="13px" height="13px">&nbsp;Phones</a>
+				<div class="dropdown-content">
+				<a href="apple.php">Apple</a>
+				<a href="att.php">AT&T</a>
+				<a href="samsung.php">Samsung</a>
+				<a href="sprint.php">Sprint</a>
+				<a href="tmobile.php">T-Mobile</a>
+				<a href="unlocked.php">Unlocked</a>
+				<a href="verizon.php">Verizon</a>
+
+				</div>
+
+			</div>	
+
+			<div class="dropdown">
+
+				<a href="laptops.php"><img src="img/laptop.png" width="13px" height="13px">&nbsp;Laptop</a>
+				<div class="dropdown-content">
+				<a href="#">MacBooks</a>
+				<a href="#">Chromebooks</a>
+				<a href="#">Windows</a>				
+				</div>
+
+			</div>	
+
+			<div class="dropdown">
+
+				<a href="#"><img src="img/camera.png" width="13px" height="13px">&nbsp;Cameras</a>
+				<div class="dropdown-content">
+				<a href="#">Action Cameras</a>
+				<a href="#">Canon</a>
+				<a href="#">DSLR Cameras</a>
+				<a href="#">Lenses</a>
+				<a href="#">Mirrorless Cameras</a>
+				<a href="#">Nikon</a>
+
+				</div>
+
+			</div>	
+
+			<div class="dropdown">
+
+				<a href="#"><img src="img/console.png" width="13px" height="13px">&nbsp;Video Games</a>
+				<div class="dropdown-content">
+				<a href="nintendo3ds.php">Nintendo 3DS</a>
+				<a href="nintendoswitch.php">Nintendo Switch</a>
+				<a href="playstation4.php">PlayStation 4</a>
+				<a href="playstationvista.php">PlayStation Vita</a>
+				<a href="retro.php">Retro</a>
+				<a href="xboxone.php">Xbox One</a>
+
+				</div>
+
+			</div>
+
+			<div class="dropdown">
+
+				<a href="#"><img src="img/tablet.png" width="13px" height="13px">&nbsp;Tablets</a>
+				<div class="dropdown-content">
+				<a href="iPad.php">iPad</a>
+				<a href="Android.php">Android</a>
+				<a href="WindowsSurface.php">Windowss/ Surface</a>
+				<a href="AmazonKindle.php">Amazon/ Kindle</a>
+
+				</div>
+
+			</div>
+			
+			<a href="Watches.php"><img src="img/clock.png" width="13px" height="13px">&nbsp;Watches</a>
+			<a href="#"><img src="img/home.png" width="13px" height="13px">&nbsp;Home Tech</a>
+
+		</section>
+
+		<hr>
+	</header>';
+	
+
+ ?>
