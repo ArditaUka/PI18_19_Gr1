3a083016564e70ab5b2cfbf82e030e79b3646a60
EgzonaVllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2019-05-24 11:14:20 +0200

    signup

--- signup.php
+++ signup.php
@@ -1,33 +1,28 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta charset="utf-8">
-		<title>Sign Up</title>
-		<link href="https://fonts.googleapis.com/css?family=Raleway:400,600i,700i,800,800i,900,900i" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="css/login.css">
+		<title>Sign up</title>
+		<link rel="stylesheet" type="text/css" href="css/loginSignup.css">
 	</head>
-	<body>
-		<div class="total container">
+	<body class="bgimg">
+		<form action="#" method="POST" id="forma">
 			
-			<div class="img" >
-				
-				<img src="img/img-01.png">
-			</div>
-			
-			
-			<div class="input">
-				<form id="registration" method="post">
-					<h1 style="color: #005960; padding-left: 20px;">SIGN UP FOR FREE</h1>
-					<input type="text" name="firstname" placeholder="First Name" id="button"><br><br>
-					<input type="text" name="lastname" placeholder="Last Name" id="button"><br><br>
-					<input type="email" name="email" placeholder="Email" id="button"><br><br>
-					<input type="password" name="password" placeholder="Password" id="button"><br><br>
-					<button id="get">GET STARTED</button><br><br>
-				</form>
-			</div>
-			
-		</div>
-		
+			<label>Full Name</label><br><br>
+			<input type="text" name="name" placeholder="Name..." id="inputat">
+			<br><br>
+			<label>Email</label><br><br>
+			<input type="email" name="email" placeholder="Email address..." id="inputat">
+			<br><br>
+			<label>Username</label><br><br>
+			<input type="text" name="username" placeholder="Username..." id="inputat">
+			<br><br>
+			<label>Password</label><br><br>
+			<input type="password" name="password" placeholder="*******************" id="inputat">
+			<br><br>
+			<label>Confirm Password</label><br><br>
+			<input type="password" name="password1" placeholder="*******************" id="inputat">
+			<br><br>
+			<button name="submit" id="btn">Sign Up</button>
+		</form>
 	</body>
 </html>
