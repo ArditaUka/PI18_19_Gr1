d727353aa08684b8d45271c0555432ea485b975a
EgzonaVllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2019-05-24 12:34:22 +0200

    login form

--- login.php
+++ login.php
@@ -4,42 +4,31 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
 		<title>Log In</title>
-		<link href="https://fonts.googleapis.com/css?family=Raleway:400,600i,700i,800,800i,900,900i" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="css/login.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/loginSignup.css">
 	</head>
 	<body>
-		<div class="total container">
-			
-			<div class="img" >
-				
-				<img src="img/img-01.png">
+		
+
+		<form action="#" method="post">
+
+			<span style="font-family: 'Roboto Condensed', sans-serif;">Login</span>
+			<br><br><br>
+			<a href="#" class="btn-login-with bg1 m-b-10">Login with Facebook</a><br>
+			<a href="#" class="btn-login-with bg2">Login with Twitter</a><br>
+
+			<div>
+			<span style="font-family: 'Roboto Condensed', sans-serif;">Login with email</span>
+			<input type="email" name="email" placeholder="Email" class="inp"><br><br>
+			<input type="password" name="password" placeholder="Password" class="inp"><br><br>
+			</div>
+
+			<div>
+				<span style="font-family: 'Roboto Condensed', sans-serif; opacity: 0.7;" >Don't have an account?</span><br>
+				<a href="signup.php" style="margin-left: 25px;">SIGN UP NOW</a>
 			</div>
 			
-			
-			<div class="input">
-				<form id="registration" method="post">
-					<h1 style="color: #005960; padding-left: 40px;">MEMBER LOGIN</h1>
-					<input type="email" name="email" placeholder="Email" id="button"><br><br>
-					<input type="password" name="password" placeholder="Password" id="button"><br><br>
-					<label style="color: black; ">
-					<input type="submit" value="Log In" id="butto"><br><br>
-					
-					<div class="text-center p-t-12">
-
-						<span class="txt1">Forgot</span>
-						<a class="txt2" href="#">Username / Password?</a>
-
-					</div>
-
-
-					<div class="a">
-						<a class="txt2" href="signup.html">Create your Account &rarr;</a>
-					</div>
-					
-				</form>
-			</div>
-			
-		</div>
+		</form>
 		
 	</body>
 </html>
