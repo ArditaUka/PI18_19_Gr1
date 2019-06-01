aa41056a4af1a829f08f441d5859c8f8b00320bd
EgzonaVllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2019-06-01 12:17:28 +0200

    google login

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+<?php
+
+require_once('config.php');
+
+$login_url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online';
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -39,14 +47,15 @@
 
 		 ?>
 		
-
-		<form action="login.php" method="post" style="margin-top: 0px;">
-
-			<span style="margin-left: 55px; font-size: 25px; font-family: 'Roboto Condensed', sans-serif;">Login</span>
-			<br><br>
-			<a href="#" class="btn-login-with bg1 m-b-10">Login with Facebook</a><br>
+		<form action="#" method="GET">
+			<span style="margin-left: 55px; margin-bottom: 10px; font-size: 25px; font-family: 'Roboto Condensed', sans-serif;">Login</span>
+			<a href="<?= $login_url ?>" class="btn-login-with bg1 m-b-10" style="margin-top: 20px;">Login with Google</a><br>			
 			<a href="#" class="btn-login-with bg2">Login with Twitter</a>
 
+		</form>
+
+		<form action="login.php" method="post" style="margin-top: -10px;">
+			
 			<div>
 			<span style="margin-left: 6px; font-size: 25px; font-family: 'Roboto Condensed', sans-serif;">Login with email</span>
 			<input type="email" name="email" value="<?php echo @$_COOKIE['email'];?>" placeholder="Email" class="inp"><br><br>
