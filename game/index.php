2846a41455595cb9a2da33d535078ee387562e49
EgzonaVllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2019-06-02 01:29:26 +0200

    index game

---
+++ game/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+session_unset();
+?>
+
+
+<html>
+
+    <head>
+
+		<title>Millionaire</title>
+		<link rel="stylesheet" type="text/css" media="all" href="homestyle.css"/>
+
+    </head>
+
+    <body>
+
+        <?php
+        $_SESSION["bank"]=0;
+        $_SESSION["question"] = 1;
+        ?>	
+
+
+        <img src="milion.svg" width="500px;" style="margin-top: 40px; margin-bottom: 30px;"  >
+		<div>
+			<h4><a href="mill1.php">Play</a></h4>
+		</div>
+
+        
+    </body>
+</html>
